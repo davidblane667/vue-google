dbb23d779c36c39ca8a940389afa3e9355e89e9b
Nikita <davidblane667@mail.ru>
2020-11-19 19:23:57 +0700

    Update

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,7 +4,6 @@
 
       <form
         class="uk-position-center"
-        @submit.prevent="submitHandler"
       >
         <fieldset class="uk-fieldset">
 
@@ -20,6 +19,7 @@
                 class="uk-input"
                 type="text"
                 v-model="inputValue"
+                @input="debounceInput"
               >
               <a
                 v-if="inputValue.length"
@@ -29,6 +29,13 @@
                 uk-icon="icon: close"
               ></a>
             </div>
+            <div
+              v-if="inputValue && message"
+              class="uk-alert"
+            >
+              {{message}}
+            </div>
+
           </div>
 
         </fieldset>
@@ -39,68 +46,73 @@
 </template>
 
 <script>
-export default {
-  name: 'search',
-  data: () => ({
-    inputValue: '',
-    isCurrentValue: false,
-    alertOpen: false
-  }),
-  methods: {
-    clearInput() {
-      this.inputValue = ''
-      this.isCurrentValue = false
-    },
-    submitHandler() {
-      const value = this.inputValue
-      switch(this.isCurrentValue) {
-        case !this.validateEmail(value):
-          alert('Вы искали Email')
-          this.clearInput()
-          break
-        case !this.validatePhone(value):
-          alert('Вы искали Телефон')
-          this.clearInput()
-          break
-        case !this.validateNickname(value):
-          alert('Вы искали Никнейм')
-          this.clearInput()
-          break
-        case !this.validateNameFam(value):
-          alert('Вы искали Имя и Фамилию')
-          this.clearInput()
-          break
-        default:
-          alert('Мы ничего не нашли :(')
-          this.clearInput()
-          break
+  const debounce = require('lodash.debounce')
+
+  export default {
+    name: 'search',
+    data: () => ({
+      inputValue: '',
+      isCurrentValue: false,
+      message: ''
+    }),
+    methods: {
+      clearInput() {
+        this.inputValue = ''
+        this.isCurrentValue = false
+        this.message = ''
+      },
+      submitHandler() {
+        const value = this.inputValue
+        switch (this.isCurrentValue) {
+          case !this.validateEmail(value):
+            this.message = 'Вы искали Email'
+            break
+          case !this.validatePhone(value):
+            this.message = 'Вы искали Телефон'
+            break
+          case !this.validateNickname(value):
+            this.message = 'Вы искали Никнейм'
+            break
+          case !this.validateNameFam(value):
+            this.message = 'Вы искали Имя и Фамилию'
+            break
+          default:
+            this.message = 'Мы ничего не нашли :('
+            break
+        }
+      },
+      validateEmail(value) {
+        const re = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i
+        return this.isCurrentValue = re.test(value)
+      },
+      validatePhone(value) {
+        const re = /^((8|\+[1-9])[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+        return this.isCurrentValue = re.test(value)
+      },
+      validateNickname(value) {
+        const re = /^(?!.*\.\.)(?!\.)(?!.*\.$)(?!\d+$)[a-zA-Z0-9.]{5,50}$/
+        return this.isCurrentValue = re.test(value)
+      },
+      validateNameFam(value) {
+        const re = /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/
+        return this.isCurrentValue = re.test(value)
       }
     },
-    validateEmail(value) {
-      const re = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i
-      return this.isCurrentValue = re.test(value)
-    },
-    validatePhone(value) {
-      const re = /^((8|\+[1-9])[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
-      return this.isCurrentValue = re.test(value)
-    },
-    validateNickname(value) {
-      const re = /^(?!.*\.\.)(?!\.)(?!.*\.$)(?!\d+$)[a-zA-Z0-9.]{5,50}$/
-      return this.isCurrentValue = re.test(value)
-    },
-    validateNameFam(value) {
-      const re = /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/
-      return this.isCurrentValue = re.test(value)
-    },
+    computed: {
+      debounceInput: function () {
+        return debounce(this.submitHandler, 1000)
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss">
   .uk-section {
     min-height: 100vh;
 
-    & .uk-input {
+    & .uk-input,
+    .uk-alert {
+      box-sizing: border-box;
       width: 400px;
     }
   }
